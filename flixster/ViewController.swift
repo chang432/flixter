914ffba7c09c321a08a5141226bdebb7d81df3e6
andre <chang432@purdue.edu>
2019-01-14 18:34:57 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Andre Chang on 1/14/19.
+//  Copyright © 2019 Andre Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
