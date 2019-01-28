be48b4ff3144540c04e9591db3f811e7e816d362
andre <chang432@purdue.edu>
2019-01-27 19:24:25 -0500

    part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Andre Chang on 1/26/19.
+//  Copyright © 2019 Andre Chang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
